7de02add0b9631975197ff2d341837b39d77b12f
hailong wu <13126506430@163.com>
2017-07-17 18:15:15 +0800

    1.组织机构的layer不能为空，否则下拉框无法显示；

---
+++ project/Elight.Web/Areas/TestCenter/Controllers/InGroupController.cs
@@ -0,0 +1,18 @@
+﻿using Elight.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Elight.Web.Areas.TestCenter.Controllers
+{
+    public class InGroupController : BaseController
+    {
+        // GET: TestCenter/InGroup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ project/Elight.Web/Areas/TestCenter/TestCenterAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Elight.Web.Areas.TestCenter
+{
+    public class TestCenterAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TestCenter";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TestCenter_default",
+                "TestCenter/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
